689afa73b2935c42d8ae6da37ca3b10aa64f3a15
lilansheng <945004642@qq.com>
2024-03-26 19:13:54 +0800

    安装Ant Design 组件库

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Button } from 'antd'
 function App() {
-  return <div className="App">app</div>
+  return (
+    <div className="App">
+      <Button type='primary'>demo</Button>
+    </div>
+  )
 }
 
 export default App
