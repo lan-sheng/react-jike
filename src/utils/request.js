cd9a252d4a53647e1fc1538e3cdbaf68fee5543f
lilansheng <945004642@qq.com>
2024-03-28 14:53:29 +0800

    Axios请求拦截器注入Token

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,13 +3,18 @@
 // 2.超时时间
 // 3.请求拦截器、响应拦截器
 import axios from 'axios'
+import { getToken } from './token'
 
 export const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
   timeout: 5000,
 })
 // 添加请求拦截器
-request.interceptors.request.use((config)=> {
+request.interceptors.request.use((config) => {
+  const token = getToken()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
     return config
   }, (error)=> {
     return Promise.reject(error)
