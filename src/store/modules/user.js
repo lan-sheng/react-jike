852dfd1a04861ea8d30dc182cdc7f7614ed15226
lilansheng <945004642@qq.com>
2024-03-28 11:15:02 +0800

    登录-使用Redux管理token

---
+++ src/store/modules/user.js
@@ -0,0 +1,18 @@
+const { createSlice } = require('@reduxjs/toolkit')
+
+const userStore = createSlice({
+  name: 'user',
+  initialState: {
+    token: '',
+  },
+  reducers: {
+    setToken(state, action) {
+      state.token = action.payload
+    },
+  },
+})
+
+const { setToken } = userStore.actions
+const userReducer = userStore.reducer
+export { setToken }
+export default userReducer
