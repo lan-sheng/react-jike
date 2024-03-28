843d311d0f28910371abcc2c67274a7fb8259ee3
lilansheng <945004642@qq.com>
2024-03-28 18:41:13 +0800

    使用Token做路由权限控制

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
+import { AuthRoute } from '@/components/AuthRoute'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
-    children: [],
+    element: (
+      <AuthRoute>
+        <Layout />
+      </AuthRoute>
+    ),
   },
   {
     path: 'login',
