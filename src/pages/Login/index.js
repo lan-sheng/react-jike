ed01e06e81ecc4e1964483fa12a79fdf8337d9f5
lilansheng <945004642@qq.com>
2024-03-28 10:05:04 +0800

    登录-获取表单数据

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,32 @@
+import { Card, Form, Input, Button, Checkbox } from 'antd'
+import logo from '@/assets/logo.png'
+import './index.scss'
+
 const Login = () => {
-  return <div>登录页</div>
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox className="login-checkbox-label">我已阅读并同意「用户协议」和「隐私条款」</Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
