b0736382f021dd51bc1d2fac98559a2929ca3081
lilansheng <945004642@qq.com>
2024-03-28 10:19:07 +0800

    登录-表单校验实现

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,16 +8,33 @@ const Login = () => {
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form>
-          <Form.Item>
+        <Form validateTrigger={['onBlur', 'onChange']}>
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '手机号码格式不对',
+                validateTrigger: 'onBlur',
+              },
+              { required: true, message: '请输入手机号' },
+            ]}
+          >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
-            <Input size="large" placeholder="请输入验证码" />
+          <Form.Item
+            name="code"
+            rules={[
+              { len: 6, message: '验证码6个字符', validateTrigger: 'onBlur' },
+              { required: true, message: '请输入验证码' },
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" maxLength={6} />
           </Form.Item>
-          <Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
             <Checkbox className="login-checkbox-label">我已阅读并同意「用户协议」和「隐私条款」</Checkbox>
           </Form.Item>
+
           <Form.Item>
             <Button type="primary" htmlType="submit" size="large" block>
               登录
