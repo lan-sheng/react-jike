fd7f92941186ca2e9a1e4ac84efbb80c84b09a79
lilansheng <945004642@qq.com>
2024-04-08 15:52:21 +0800

    文章列表模块-适配文章状态

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -16,6 +16,11 @@ const { Option } = Select
 const { RangePicker } = DatePicker
 
 const Article = () => {
+  const status = {
+    1: <Tag color="warning">待审核</Tag>,
+    2: <Tag color="success">审核通过</Tag>,
+  }
+
   const columns = [
     {
       title: '封面',
@@ -33,7 +38,7 @@ const Article = () => {
     {
       title: '状态',
       dataIndex: 'status',
-      render: data => <Tag color="green">审核通过</Tag>,
+      render: data => status[data],
     },
     {
       title: '发布时间',
