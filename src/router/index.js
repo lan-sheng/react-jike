7d666c441578ef508af31f0f31678c6b03698a2b
lilansheng <945004642@qq.com>
2024-04-09 16:13:50 +0800

    打包优化-配置路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,13 @@ import { createBrowserRouter } from 'react-router-dom'
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import { AuthRoute } from '@/components/AuthRoute'
-import Home from '@/pages/Home'
-import Article from '@/pages/Article'
-import Publist from '@/pages/Publish'
-
+// import Home from '@/pages/Home'
+// import Article from '@/pages/Article'
+// import Publist from '@/pages/Publish'
+import { Suspense, lazy } from 'react'
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publist = lazy(() => import('@/pages/Publish'))
 const router = createBrowserRouter([
   {
     path: '/',
@@ -18,15 +21,27 @@ const router = createBrowserRouter([
       {
         path: 'home',
         index: true,
-        element: <Home />,
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Home />
+          </Suspense>
+        ),
       },
       {
         path: 'article',
-        element: <Article />,
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Article />
+          </Suspense>
+        ),
       },
       {
         path: 'publist',
-        element: <Publist />,
+        element: (
+          <Suspense fallback={'加载中'}>
+            <Publist />
+          </Suspense>
+        ),
       },
     ],
   },
